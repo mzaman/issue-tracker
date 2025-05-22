a716711ea03ed450498947c9d734672608412835
Masud Zaman <masud.zmn@gmail.com>
2025-05-23 00:47:14 +0600

    Task 5: Add feature on authentication (User Login and generate token)

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 'use strict';
 
+require('dotenv').config();
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 
-const config = require('./config');
+const { jwtMiddleware, clientIdMiddleware } = require('./middleware/auth');
 const router = require('./lib/routes');
+const config = require('./config');
 
 const app = new Koa();
 
 app.use(bodyParser());
+app.use(clientIdMiddleware);
+app.use(jwtMiddleware);
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen(config.port);
-console.log('Listening on http://localhost:%s/', config.port);
+console.log(`Listening on http://localhost:${config.port}`);
