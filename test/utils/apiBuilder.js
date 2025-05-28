ff8c8d757403ce9cf89391fa7f4fdf23e8731dbd
Masud Zaman <masud.zmn@gmail.com>
2025-05-28 08:14:20 +0600

    [Test]: add ApiBuilder utility for fluent API request construction
    - Introduced ApiBuilder class to simplify HTTP request building in tests
    - Supports setting headers, cookies, query params, content type, and payloads
    - Enables chaining methods for cleaner and reusable test code
    - Handles automatic expectation of response status and resets after each request

---
+++ test/utils/apiBuilder.js
@@ -0,0 +1,89 @@
+// test/utils/apiBuilder.js
+
+class ApiBuilder {
+    constructor(baseRequest, defaultHeaders = {}) {
+        this.baseRequest = baseRequest;
+        this.defaultHeaders = defaultHeaders;
+        this.reset();
+    }
+
+    reset() {
+        this.headers = {};
+        this.cookies = '';
+        this.queryParams = null;
+        this.expectCode = null;
+        this.payload = null;
+        this.contentType = 'application/json';
+        return this;
+    }
+
+    withHeaders(headers) {
+        this.headers = { ...this.headers, ...headers };
+        return this;
+    }
+
+    withCookie(cookieString) {
+        this.cookies += (this.cookies ? '; ' : '') + cookieString;
+        return this;
+    }
+
+    withQuery(params) {
+        this.queryParams = params;
+        return this;
+    }
+
+    withContentType(type) {
+        this.contentType = type;
+        return this;
+    }
+
+    sendJson(data) {
+        this.payload = data;
+        this.contentType = 'application/json';
+        return this;
+    }
+
+    expectStatus(code) {
+        this.expectCode = code;
+        return this;
+    }
+
+    async _buildAndSend(method, url) {
+        let req = this.baseRequest[method](url);
+
+        const combinedHeaders = { ...this.defaultHeaders, ...this.headers };
+        for (const [key, value] of Object.entries(combinedHeaders)) {
+            req.set(key, value);
+        }
+
+        if (this.cookies) {
+            req.set('Cookie', this.cookies);
+        }
+
+        if (this.queryParams) {
+            req.query(this.queryParams);
+        }
+
+        if (this.payload) {
+            req.set('Content-Type', this.contentType);
+            req.send(this.payload);
+        }
+
+        if (this.expectCode) {
+            req.expect(this.expectCode);
+        }
+
+        const res = await req;
+
+        this.reset();
+        return res;
+    }
+
+    get(url) { return this._buildAndSend('get', url); }
+    post(url) { return this._buildAndSend('post', url); }
+    put(url) { return this._buildAndSend('put', url); }
+    patch(url) { return this._buildAndSend('patch', url); }
+    delete(url) { return this._buildAndSend('delete', url); }
+}
+
+module.exports = ApiBuilder;
